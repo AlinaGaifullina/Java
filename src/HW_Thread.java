9974a35159d01754f9094ce184fb8dd3f3f78e4e
AlinaGaifullina <90114019+AlinaGaifullina@users.noreply.github.com>
2022-05-14 11:36:20 +0300

    Update HW_Thread.java

--- src/HW_Thread.java
+++ src/HW_Thread.java
@@ -2,46 +2,43 @@ import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class HW_Thread extends JFrame implements KeyListener{
+import java.io.IOException;
 
-    //не придумала, как это сделать без окна, поэтому вот оно:
-    public HW_Thread(){
-        setSize(1,1);
-        setVisible(true);
-        addKeyListener(this);
-    }
-
+public class HW_Thread {
     public static void main(String[] args) {
-        HW_Thread p = new HW_Thread();
-        Thread1 thread = new Thread1();
-        thread.start();
-    }
-
-    public static class Thread1 extends Thread{
-        @Override
-        public void run() {
-            while(flag){
-                System.out.println("TEST MESSAGE");
+        Thread message = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    System.out.println("TEST MESSAGE");
+                    try {
+                        Thread.sleep(800);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-        }
+        });
+
+        Thread closeMessage = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        int code = System.in.read();
+                        if (code == 10) {
+                            System.exit(0);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
+
+        message.start();
+        closeMessage.start();
+
     }
-
-    static boolean flag = true;
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        if(e.getKeyCode()==KeyEvent.VK_ENTER){
-            flag = false;
-            System.out.println("Поток остановлен");
-        } else flag = true;
-    }
-
-
-    //не используем:
-
-    @Override
-    public void keyTyped(KeyEvent e) {}
-
-    @Override
-    public void keyReleased(KeyEvent e) {}
 }
